f47b8e4db64393c837508ab2f50d23abb5e69ae1
Ethan A. Smith <smithea3@outlook.com>
2022-05-10 08:37:58 -0400

    Updated preview file

--- src/templates/preview.ts
+++ src/templates/preview.ts
@@ -1,7 +1,8 @@
 const renderPreview = `
-  <h4>Syllabus for {{ coursename }}</h4>
+  <h2 style="border-bottom: 2px solid; text-align: center">Syllabus for {{ coursename }}</h2>
   <p>{{ introduction }}</p>
 
+  <h3 style="border-bottom: 2px solid;">Class Meeting Dates</h3>
   {% for meeting in meetings -%}
   <h5>{{ meeting }}</h5>
 
